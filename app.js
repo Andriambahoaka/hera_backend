c8468172ca180d6cad4cb9347efcc00ea27df96f
Mahery Joe <mahery@futurmap.com>
2025-02-17 15:52:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://maheryj23:He06zvvoqJSlT88s@cluster0.xklfy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+
+//progiciel 
+//Intergiciels (« Middleware »)¶ Les intergiciels représentent un système de points d'entrée dans le traitement des requêtes et des réponses
+
+//La méthode app.use() vous permet d'attribuer un middleware à une route spécifique de votre application.
+
+//Le CORS définit comment les serveurs et les navigateurs interagissent, en spécifiant quelles ressources 
+// peuvent être demandées de manière légitime – par défaut, 
+// les requêtes AJAX sont interdites
+
+// pas de routes afin de s'appliquer à toutes les routes
+app.use((req, res, next) => {
+  //d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  //d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  //d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const app = require('./app');
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port);
+// CORS : Cross Origin Ressource Shraring 
+// Nécéssaire à partir du moment ou le back end and front end ne partage pas la même origine
+// En tetes http, un système de sécurité pour bloquer les requetes malveillants
+
+//CORS signifie « Cross Origin Resource Sharing ».
+// Il s'agit d'un système de sécurité qui, par défaut, 
+// bloque les appels HTTP entre des serveurs différents,
+ // ce qui empêche donc les requêtes malveillantes
+ //  d accéder à des ressources sensibles auquels ils n'ont pas le droit d'accès. Dans notre cas,
+ ////   nous avons deux origines : localhost:3000 et localhost:4200 , 
+   // et nous souhaiterions qu'elles puissent communiquer entre elles. 
+   // Pour cela, nous devons ajouter des headers à notre objet  response .
