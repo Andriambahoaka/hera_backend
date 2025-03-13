4077cf7ad004b68a38abe5e1b9e907f8a52a469c
Mahery Joe <mahery@futurmap.com>
2025-03-13 18:02:12 +0300

    implement  add access pack

--- controllers/pack.js
+++ controllers/pack.js
@@ -1,4 +1,5 @@
 const Pack = require('../models/Pack');
+const PackAccess = require('../models/PackAccess');
 
 // Controller function to add a pack with devices
 exports.addPack = (req, res, next) => {
@@ -27,3 +28,48 @@ exports.addPack = (req, res, next) => {
         .then(() => res.status(201).json({ message: 'Pack created successfully', newPack }))
         .catch(error => res.status(400).json({ error: 'Error creating pack: ' + error.message }));
 };
+
+// Controller function to get all packs
+exports.getAllPacks = (req, res, next) => {
+    // Use Mongoose to find all packs in the database
+    Pack.find()
+        .then(packs => {
+            // If packs are found, return them
+            res.status(200).json({ message: 'Packs retrieved successfully', packs });
+        })
+        .catch(error => {
+            // If an error occurs, return an error message
+            res.status(500).json({ error: 'Error retrieving packs: ' + error.message });
+        });
+};
+
+
+exports.addPackAccess = async (req, res) => {
+    const { userId, packId, access } = req.body;
+  
+    // Check if required fields are provided
+    if (!userId || !packId || !access) {
+      return res.status(400).json({ message: "userId, packId, and access are required" });
+    }
+  
+    try {
+      // Create a new PackAccess document
+      const newPackAccess = new PackAccess({
+        userId,
+        packId,
+        access
+      });
+  
+      // Save the new PackAccess to the database
+      await newPackAccess.save();
+  
+      // Return success response
+      res.status(201).json({
+        message: "PackAccess added successfully!",
+        packAccess: newPackAccess
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error adding PackAccess", error });
+    }
+  };
--- routes/pack.js
+++ routes/pack.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const packCtrl = require('../controllers/pack');
 
 router.post('/',packCtrl.addPack);
+router.get('/',packCtrl.getAllPacks);
+router.post('/access',packCtrl.addPackAccess);
 
 module.exports = router;
