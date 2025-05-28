ab96673e8aaf043d2719e41803aff2b56e3352d0
DESKTOP-NIUC80S\maher <maheryj23@gmail.com>
2025-05-28 23:30:09 +0300

    update pack object

--- controllers/pack.js
+++ controllers/pack.js
@@ -3,65 +3,58 @@ const PackAccess = require('../models/PackAccess');
 
 // Controller function to add a pack with devices
 exports.addPack = (req, res, next) => {
-    const { ownerId, name, devices } = req.body;
+  const { ownerId, deviceId, devicePassword } = req.body;
 
-    // Check if required fields are provided
-    if (!name || !devices || !Array.isArray(devices)) {
-        return res.status(400).json({ message: 'name and devices are required, and devices must be an array' });
-    }
+  // Check if required fields are provided
+  if (!ownerId || !deviceId || !devicePassword) {
+    return res.status(400).json({ message: 'OwnerId , deviceId and devicePassword are required' });
+  }
+  
+  // Create a new Pack
+  const newPack = new Pack({
+    ownerId: ownerId,
+    deviceId: deviceId,
+    devicePassword: devicePassword
+  });
 
-    // Check if devices array contains all required fields
-    for (let device of devices) {
-        if (!device.deviceId || !device.devicePassword) {
-            return res.status(400).json({ message: 'Each device must have deviceId, devicePassword' });
-        }
-    }
-
-    // Create a new Pack
-    const newPack = new Pack({
-        ownerId: ownerId,
-        name,
-        devices: devices
-    });
-
-    // Save the new pack to the database
-    newPack.save()
-        .then(() => res.status(201).json({ message: 'Pack created successfully', newPack }))
-        .catch(error => res.status(400).json({ error: 'Error creating pack: ' + error.message, newPack }));
+  // Save the new pack to the database
+  newPack.save()
+    .then(() => res.status(201).json({ message: 'Pack created successfully', newPack }))
+    .catch(error => res.status(400).json({ error: 'Error creating pack: ' + error.message, newPack }));
 };
 
 // Controller function to get all packs
 exports.findAll = (req, res, next) => {
-    // Use Mongoose to find all packs in the database
-    Pack.find()
-        .then(packs => {
-            // If packs are found, return them
-            res.status(200).json({ message: 'Packs retrieved successfully', packs });
-        })
-        .catch(error => {
-            // If an error occurs, return an error message
-            res.status(500).json({ error: 'Error retrieving packs: ' + error.message });
-        });
+  // Use Mongoose to find all packs in the database
+  Pack.find()
+    .then(packs => {
+      // If packs are found, return them
+      res.status(200).json({ message: 'Packs retrieved successfully', packs });
+    })
+    .catch(error => {
+      // If an error occurs, return an error message
+      res.status(500).json({ error: 'Error retrieving packs: ' + error.message });
+    });
 };
 
 
 exports.findAllByOwner = (req, res, next) => {
-    const { ownerId } = req.params; // Récupération de l'ID du propriétaire depuis les paramètres de la requête
+  const { ownerId } = req.params; // Récupération de l'ID du propriétaire depuis les paramètres de la requête
 
-    if (!ownerId) {
-        return res.status(400).json({ message: "L'ownerId est requis." });
-    }
+  if (!ownerId) {
+    return res.status(400).json({ message: "L'ownerId est requis." });
+  }
 
-    Pack.find({ ownerId })
-        .then(packs => {
-            if (packs.length === 0) {
-                return res.status(404).json({ message: "Aucun pack trouvé pour cet ownerId." });
-            }
-            res.status(200).json({ message: 'Packs récupérés avec succès', packs });
-        })
-        .catch(error => {
-            res.status(500).json({ error: 'Erreur lors de la récupération des packs: ' + error.message });
-        });
+  Pack.find({ ownerId })
+    .then(packs => {
+      if (packs.length === 0) {
+        return res.status(404).json({ message: "Aucun pack trouvé pour cet ownerId." });
+      }
+      res.status(200).json({ message: 'Packs récupérés avec succès', packs });
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Erreur lors de la récupération des packs: ' + error.message });
+    });
 };
 
 
--- models/Pack.js
+++ models/Pack.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose');
 
-const deviceSchema = mongoose.Schema({
-    deviceId: { type: String, required: true, unique: true },
-    devicePassword: { type: String, required: true },
-    deviceType: { type: String }
-});
-
 const packSchema = mongoose.Schema({
     ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required : true},
-    name: { type: String, required: true },
-    devices: [deviceSchema]
+    deviceId: { type: String, required: true, unique: true },
+    devicePassword: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Pack', packSchema);
