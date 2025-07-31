8d710b96d0f16a2e6137e186d7ee8cca37bbf6a4
Mahery Joe <maheryj23@gmail.com>
2025-07-31 17:00:27 +0300

    fix upload directory

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,6 +1,12 @@
 const multer = require('multer');
 const path = require('path');
 
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, uploadDir),
   filename: (req, file, cb) => {
