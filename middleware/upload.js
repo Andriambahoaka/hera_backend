26da12d8e1a1470911a05cec05d9d5ad0925dfb3
Mahery Joe <maheryj23@gmail.com>
2025-08-01 16:31:27 +0300

    feat : use memory storage

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,19 +2,8 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const uploadDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, uploadDir),
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const filename = `${req.params.id}_${Date.now()}${ext}`;
-    cb(null, filename);
-  }
-});
+// Use memory storage so file is available in req.file.buffer
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
